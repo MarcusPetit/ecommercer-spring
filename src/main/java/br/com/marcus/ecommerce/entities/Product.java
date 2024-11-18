49b66f5263e257c5682cd58ccfd24c51f9e1644c
Marcus <marcuspetit@hotmail.com>
2024-11-18 04:57:24 -0300

    criando products e categories com manytomany juntando em uma tabela auxiliar

---
+++ src/main/java/br/com/marcus/ecommerce/entities/Product.java
@@ -0,0 +1,21 @@
+package br.com.marcus.ecommerce.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
